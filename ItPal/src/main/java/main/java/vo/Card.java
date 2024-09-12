e09e5e7f067534f35067928210c249b8530e7f88
Heeju Lee <heejulia21@naver.com>
2024-09-12 10:12:22 +0900

    [BE] add : VO 추가

---
+++ ItPal/src/main/java/main/java/vo/Card.java
@@ -0,0 +1,61 @@
+package main.java.vo;
+
+public class Card {
+	private String cardName;
+	private String imgUrl;
+	private String cUrl;
+	private String companyName;
+	private String cardDetail;
+	private int annualFee;
+	private String cateType;
+	private Benefit benefit;
+	
+	public Card() {}
+	
+	public Card(String cardName, String imgUrl, String cUrl, String companyName, String cardDetail, int annualFee, String cateType,
+			Benefit benefit) {
+		this.cardName = cardName;
+		this.imgUrl = imgUrl;
+		this.cUrl = cUrl;
+		this.companyName = companyName;
+		this.cardDetail = cardDetail;
+		this.annualFee = annualFee;
+		this.cateType = cateType;
+		this.benefit = benefit;
+	}
+	
+	public String getCardName() {
+		return cardName;
+	}
+	
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getcUrl() {
+		return cUrl;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public String getCardDetail() {
+		return cardDetail;
+	}
+
+	public int getAnnualFee() {
+		return annualFee;
+	}
+
+	public String getCateType() {
+		return cateType;
+	}
+
+	public Benefit getBenefit() {
+		return benefit;
+	}
+	
+	
+	
+}
