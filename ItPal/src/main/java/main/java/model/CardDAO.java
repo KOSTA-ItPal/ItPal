f95a5d0df55d0700eb8a08d85c8284304c14bb73
Shin-seung-hyun <jjack1028@naver.com>
2024-09-12 19:03:34 +0900

    [BE] add : DAO 추가

---
+++ ItPal/src/main/java/main/java/model/CardDAO.java
@@ -0,0 +1,29 @@
+package main.java.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import main.java.model.vo.Card;
+import main.java.model.vo.User;
+
+public interface CardDAO {
+	
+	// 공통 로직
+	Connection getConnect() throws SQLException;
+	void closeAll(PreparedStatement ps, Connection conn)throws SQLException;
+	void closeAll(ResultSet rs,PreparedStatement ps, Connection conn)throws SQLException;
+	
+/*CARD 1 : 카드 조회*/	
+	ArrayList<Card> showNewCards()throws SQLException;
+	ArrayList<Card> showHotCards()throws SQLException;
+	ArrayList<Card> showAllCards(String cardType)throws SQLException;
+	ArrayList<Card> searchByCategory(String category)throws SQLException;
+	
+/*CARD 2 : 카드 추천 3개만 */
+	ArrayList<ArrayList<Card>> searchByCategory(User user)throws SQLException;
+
+
+}
