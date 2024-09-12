ef63d2d166a8328562896b348efb7d952c313f29
Shin-seung-hyun <jjack1028@naver.com>
2024-09-12 10:55:03 +0900

    [SQL] fix : sql 문 수정

--- ItPal/src/main/java/sql/itpal.sql
+++ ItPal/src/main/java/sql/itpal.sql
@@ -1,4 +1,46 @@
+-- -----------------------------------------------------
+-- Table `itpal`.`User`
+-- -----------------------------------------------------
+CREATE TABLE `User` (
+  `user_id` VARCHAR(45) NOT NULL,
+  `user_name` VARCHAR(45) NULL,
+  `user_pwd` VARCHAR(45) NULL,
+  `phone_num` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `birthday` DATE NULL,
+  PRIMARY KEY (`user_id`)
+  );
+  
+-- -----------------------------------------------------
+-- Table `itpal`.`Payment`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `itpal`.`Payment` (
+  `P_id` INT auto_increment,
+  `date` DATE NULL,
+  `card_num` VARCHAR(45) NULL,
+  `store` VARCHAR(45) NULL,
+  `pay` INT NULL,
+  `category` VARCHAR(45) NULL,
+  `User_user_id` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`P_id`),
+  CONSTRAINT `fk_Payment_User`
+    FOREIGN KEY (`User_user_id`)
+    REFERENCES `itpal`.`User` (`user_id`));
 
+-- -----------------------------------------------------
+-- Table `itpal`.`Account`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `itpal`.`Account` (
+  `account_num` VARCHAR(45),
+  `account_type` VARCHAR(45) NULL,
+  `bank_name` VARCHAR(45) NULL,
+  `balance` INT NULL,
+  `User_user_id` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`account_num`, `User_user_id`),
+  CONSTRAINT `fk_Account_User1`
+    FOREIGN KEY (`User_user_id`)
+    REFERENCES `itpal`.`User` (`user_id`));
+    
 -- -----------------------------------------------------
 -- Table `itpal`.`Bank`
 -- -----------------------------------------------------
@@ -13,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `itpal`.`Bank` (
 -- Table `itpal`.`Deposit`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `itpal`.`Deposit` (
-  `D_id` INT NOT NULL,
+  `d_id` INT auto_increment,
   `product_name` VARCHAR(45) NOT NULL,
   `pre_tax_rate` DECIMAL NOT NULL,
   `after_tax_rate` DECIMAL NOT NULL,
@@ -24,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `itpal`.`Deposit` (
   `register_type` VARCHAR(45) NOT NULL,
   `deposit_type` VARCHAR(45) NOT NULL,
   `Bank_bank_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`D_id`, `Bank_bank_name`),
+  PRIMARY KEY (`D_id`),
   INDEX `fk_Deposit_Bank1_idx` (`Bank_bank_name` ASC) VISIBLE,
   CONSTRAINT `fk_Deposit_Bank1`
     FOREIGN KEY (`Bank_bank_name`)
@@ -37,6 +79,7 @@ CREATE TABLE IF NOT EXISTS `itpal`.`Deposit` (
 -- Table `itpal`.`Card`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `itpal`.`Card` (
+  `c_id` int auto_increment,
   `card_name` VARCHAR(45) NOT NULL,
   `img_url` VARCHAR(200) NULL,
   `c_url` VARCHAR(200) NULL,
@@ -44,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `itpal`.`Card` (
   `card_detail` VARCHAR(200) NULL,
   `annual_fee` INT NULL,
   `card_type` VARCHAR(45) NULL,
-  PRIMARY KEY (`card_name`)
+  PRIMARY KEY (`c_id`)
   );
 
 
@@ -64,60 +107,4 @@ CREATE TABLE IF NOT EXISTS `itpal`.`Benefit` (
   PRIMARY KEY (`Card_card_name`),
   CONSTRAINT `fk_Benefit_Card1`
     FOREIGN KEY (`Card_card_name`)
-    REFERENCES `itpal`.`Card` (`card_name`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `itpal`.`User`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `itpal`.`User` (
-  `user_id` VARCHAR(45) NOT NULL,
-  `user_name` VARCHAR(45) NULL,
-  `user_pwd` VARCHAR(45) NULL,
-  `phone_num` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `birthday` DATE NULL,
-  PRIMARY KEY (`user_id`)
-  );
-
-
--- -----------------------------------------------------
--- Table `itpal`.`Payment`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `itpal`.`Payment` (
-  `P_id` INT NOT NULL,
-  `date` DATE NULL,
-  `card_num` VARCHAR(45) NULL,
-  `store` VARCHAR(45) NULL,
-  `pay` INT NULL,
-  `category` VARCHAR(45) NULL,
-  `User_user_id` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`P_id`, `User_user_id`),
-  INDEX `fk_Payment_User_idx` (`User_user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Payment_User`
-    FOREIGN KEY (`User_user_id`)
-    REFERENCES `itpal`.`User` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-    );
-
-
--- -----------------------------------------------------
--- Table `itpal`.`Account`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `itpal`.`Account` (
-  `account_num` VARCHAR(45) NOT NULL,
-  `account_type` VARCHAR(45) NULL,
-  `bank_name` VARCHAR(45) NULL,
-  `balance` INT NULL,
-  `User_user_id` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`account_num`, `User_user_id`),
-  INDEX `fk_Account_User1_idx` (`User_user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Account_User1`
-    FOREIGN KEY (`User_user_id`)
-    REFERENCES `itpal`.`User` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+    REFERENCES `itpal`.`Card` (`card_name`));
